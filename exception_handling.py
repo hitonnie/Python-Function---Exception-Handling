df565caacde5c47d895adcd88edf54e998fdc16c
hitonnie <161940723+hitonnie@users.noreply.github.com>
2025-07-05 16:28:22 +0200

    Add files via upload

---
+++ exception_handling.py
@@ -0,0 +1,12 @@
+
+try:
+    print(x)
+except NameError:
+    print("Variable x is not defined")
+finally:
+    print("Everything went wrong")
+    
+x = -1
+
+if x < 0:
+    raise Exception("sorry, no numbers below zero")
---
+++ modules_buitin.py
@@ -0,0 +1,12 @@
+import datetime
+import math
+import random
+import os
+import sys
+
+#print(math.pi)
+#print(random.randint(0, 100))
+#print(datetime.datetime.now())
+#print(os.getcwd())
+print(sys.version)
+
